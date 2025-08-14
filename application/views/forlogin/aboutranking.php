3c89193a651aadb439584e6c8207db499340fd69
Ivan Brilata <brilataivan86@gmail.com>
2025-08-14 18:56:39 +0800

    latest update

--- application/views/forlogin/aboutranking.php
+++ application/views/forlogin/aboutranking.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>About Us | Your Platform</title>
+    <title>About Usz| Your Platform</title>
 
     <!-- Bootstrap 5 CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
